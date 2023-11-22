09fe9fa3090921318a0ba32a32060e3d870fce4c
Voke Elstein <141534071+rainytides@users.noreply.github.com>
2023-11-21 23:59:50 -0800

    auth edits

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
-// Import necessary modules and middleware
+'use strict';
+
 const express = require('express');
 const router = express.Router();
 const bcryptjs = require('bcryptjs');
@@ -9,9 +10,11 @@ const asyncHandler = require('./middleware/asyncHandler');
 // Helper function to handle route errors
 function routeErrorHandler(error, res) {
     if (error.name === 'SequelizeValidationError') {
+        // Handle validation errors from Sequelize
         const errors = error.errors.map(err => err.message);
         res.status(400).json({ errors });
     } else {
+        // Throw the error to be caught by the global error handler
         throw error;
     }
 }
@@ -31,17 +34,21 @@ router.post('/', asyncHandler(async (req, res) => {
         const user = req.body;
         // Hash the password if provided
         if (user.password) {
-            user.password = bcryptjs.hashSync(user.password, 10); // The second argument is the salt length
+            user.password = bcryptjs.hashSync(user.password, 10);
         }
 
-        // Create a new user with the hashed password
+        // Create a new user
         await User.create(user);
-        // Set the location header to the root and return a 201 status
         res.status(201).location('/').end();
     } catch (error) {
-        routeErrorHandler(error, res);
+        // Handle unique constraint error for email address
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            res.status(400).json({ message: 'The email address is already in use. Please use a different email address.' });
+        } else {
+            routeErrorHandler(error, res);
+        }
     }
 }));
 
-// Export the router for use in the main app
+// Export the router for use in the main application
 module.exports = router;
